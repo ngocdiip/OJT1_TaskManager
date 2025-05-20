b28ced8b224099ddbda551a358c4e1b45c2fe45d
Khuất Trường Huy <cloneacc241@gmail.com>
2025-05-20 16:56:13 +0700

    header added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import Header from "./components/header/header.jsx";
 import Home from "./pages/Home.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 
-import Task from "./pages/TaskManager.jsx"
+import Task from "./pages/TaskManager.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
